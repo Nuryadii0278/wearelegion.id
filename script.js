3114307c8a097e81c7db786109efba4343921518
Nuryadii0278 <133551784+Nuryadii0278@users.noreply.github.com>
2025-01-29 19:18:48 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Ambil elemen tombol dengan ID
+const exploreButton = document.getElementById("exploreButton");
+
+// Tambahkan event listener untuk klik tombol
+exploreButton.addEventListener("click", () => {
+  // Redirect ke halaman explore.html
+  window.location.href = "explore.html";
+});
